732da575e88511efea876cbec977f5f917eb9fb7
Assem <assem.moh98@gmail.com>
2023-07-05 18:36:33 +0300

    getReservedDate function

--- frontend/src/components/CustomerCardsContainer.jsx
+++ frontend/src/components/CustomerCardsContainer.jsx
@@ -4,54 +4,53 @@ import { CustomerCard } from './CustomerCard'
 export function CustomerCardsContainer() {
 
     let [services, setServices] = useState([])
+    let [all, setAll] = useEffect(false)
 
-    useEffect(() => {
-        getCards()
-    }, [])
+    // useEffect(() => {
+    //     getCards()
+    // }, [])
 
-    let getCards = async () => {
-        setServices([
-            {
-                id: 1,
-                name: 'hall',
-                url: 'hall.png'
-            },
-            {
-                id: 2,
-                name: 'car',
-                url: 'car.png'
-            },
-            {
-                id: 3,
-                name: 'photosession',
-                url: 'photosession.png'
-            },
-        ])
-
-        let response = await fetch('http://127.0.0.1:8000/service/all-services',
+    let getCards = async (url) => {
+        let response = await fetch(`http://127.0.0.1:8000/service/all-services`,
         {
             method:'GET',
         }
         )
         let data = await response.json()
-
         if(response.status === 200){
             setServices(data['results'])
-            // console.log(data['results'])
         }   
         else{
             console.log("failed")
         }
+    }
 
+
+    let showAll = async () => {
+        setAll(true)
+        await getCards("service/all-services")
+    }
+    let showMine = async () => {
+        setAll(false)
+        await getCards("view-package-services/")
     }
 
   return (
     <div>
         <div className="container">
-            <div className="row justify-content-center p-5 g-3">
-                {services.map( (_service) => {
+            <div className="row justify-content-center p-5 g-3 my-5">
+                <div className='text-center w-25 '>
+                    <button onClick={showAll} className='btn btn-primary'>Show All</button>
+                    <button onClick={showMine} className='btn btn-primary'>show my</button>
+                </div>
+            </div>
+            <div className="row justify-content-center p-5 g-3 bg-danger">
+                {all ? (services.map( (_service) => {
                     return <CustomerCard key={_service.id} service={_service} images={_service.images} />
-                })}
+                })) :
+                (services.map( (_service) => {
+                    {/* return <CustomerCard key={_service.id} service={_service} images={_service.images} /> */}
+                }))}
             </div>
         </div>
     </div>
